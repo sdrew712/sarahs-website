739877d337a38182c9d08cdcfd7608d42b9b552f
sdrew712 <drew.sarah.e@gmail.com>
2021-05-26 22:41:46 -0400

    added paragraphs, changed bg colors

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,17 @@ import BongoCat from '../components/bongo-cat'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Sarah's Website</title>
+      <Head> 
+        <title>
+          Sarah's Website
+          </title>
         <meta name="description" content="I'm Sarah and I'm a better developer than Andrew" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Welcome to my website</div>
+      
+      <h1>Welcome to my website!</h1>
+      <p id="one">I'm Sarah Drew, a developer from the bEast Coast. Here you can find all of my creative projects.</p>
+      <p id="two">Kick back and have a look around!</p>
       <BongoCat width="40%" height="40%"/>
     </div>
   )
